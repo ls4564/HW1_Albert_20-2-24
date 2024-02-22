1e36167eddc930ebfa54876ebef9f4d537503936
Lior1254 <liorshemtov2008@gmail.com>
2024-02-22 14:52:53 +0200

    #2

--- app/src/main/java/com/example/hw1_albert_20_2_24/MainActivity.java
+++ app/src/main/java/com/example/hw1_albert_20_2_24/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hw1_albert_20_2_24;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
@@ -10,8 +12,7 @@ import android.widget.ToggleButton;
 public class MainActivity extends AppCompatActivity {
     Switch sw;
     ToggleButton toogBtn;
-    boolean boolSw;
-    boolean boolToogBtn;
+    ConstraintLayout cl;
 
 
     @Override
@@ -20,27 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         sw = findViewById(R.id.sw);
         toogBtn = findViewById(R.id.toogBtn);
+        cl = findViewById(R.id.cl);
+
     }
 
     public void mainBtn(View view) {
         if(sw.isChecked() && toogBtn.isChecked() )
         {
-
+            cl.setBackgroundColor(Color.GREEN);
         }
 
         if(!sw.isChecked() && toogBtn.isChecked() )
         {
-
+            cl.setBackgroundColor(Color.RED);
         }
 
         if(sw.isChecked() && !toogBtn.isChecked() )
         {
-
+            cl.setBackgroundColor(Color.MAGENTA);
         }
 
         if(!sw.isChecked() && !toogBtn.isChecked() )
         {
-
+            cl.setBackgroundColor(Color.CYAN);
         }
 
     }
